131c851545dd34ef7f67e3f5c9351f60dac4f376
Gerry Weißbach <gerry.w@gammaproduction.de>
2015-01-23 15:08:48 +0100

    Invers logic
    This made the popupviewer work incorrectly.

--- script.js
+++ script.js
@@ -672,7 +672,7 @@
 
 	$(function(){
 	
-	    if ( typeof $.fn.naturalWidth == 'undefined' && typeof $.fn.naturalHeight == 'undefined' ) { return; }
+	    if ( typeof $.fn.naturalWidth != 'undefined' && typeof $.fn.naturalHeight != 'undefined' ) { return; }
 	
 		function img(url) { var i = new Image(); i.src = url; return i; }
 		if ('naturalWidth' in (new Image())) {
