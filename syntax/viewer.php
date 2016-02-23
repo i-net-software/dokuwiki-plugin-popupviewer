252878ea9b67e9760d1f2fc41bb670af655a2401
lisps <lisps@users.noreply.github.com>
2016-02-23 14:30:18 +0100

    fix relative media IDs

--- syntax/viewer.php
+++ syntax/viewer.php
@@ -67,7 +67,7 @@ class syntax_plugin_popupviewer_viewer extends DokuWiki_Syntax_Plugin {
         if ( !empty($id) ) {
             $origID = $id;
             resolve_mediaid(getNS($ID),$id,$exists);
-            if ( !$exsits ) {
+            if ( !$exists ) {
                 $id = $origID;
                 resolve_pageid(getNS($ID),$id);
             }
